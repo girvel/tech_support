21c194d066fb1d103755343a4a3cd4f71f03fa15
girvel <widauka@ya.ru>
2026-07-12 13:24:18 +0500

    ECS sketch

--- nob.c
+++ nob.c
@@ -11,9 +11,16 @@ int main(int argc, char **argv)
 
     bool run = false;
     flag_bool_var(&run, "run", false, "Run the program after compilation.");
+    bool debug = false;
+    flag_bool_var(&debug, "debug", false, "Run the program with GDB after compilation.");
 
     if (!flag_parse(argc, argv)) {
         flag_print_error(stderr);
+        return 1;
+    }
+
+    if (run && debug) {
+        fprintf(stderr, "Both -run and -debug provided\n");
         return 1;
     }
 
@@ -24,17 +31,23 @@ int main(int argc, char **argv)
     nob_cc_inputs(&cmd, "src/main.c");
     nob_cc_output(&cmd, ".build/main.o");
     nob_cc_flags(&cmd);
-    cmd_append(&cmd, "-I./lib/");
+    cmd_append(&cmd, "-I./lib/", "-I./src/");
+    if (debug) {
+        cmd_append(&cmd, "-ggdb");
+    }
     if (!nob_cmd_run(&cmd)) return 1;
 
     nob_cc(&cmd);
     nob_cc_inputs(&cmd, ".build/main.o");
     nob_cc_output(&cmd, "tech_support");
-    cmd_append(&cmd, "-L./lib/", "-l:libraylib.a", "-lm");
+    cmd_append(&cmd, "-L./lib/", "-l:libraylib.a", "-lGL", "-lm", "-lpthread", "-ldl", "-lX11");
     if (!nob_cmd_run(&cmd)) return 1;
 
     if (run) {
         cmd_append(&cmd, "./tech_support");
+        if (!cmd_run(&cmd)) return 1;
+    } else if (debug) {
+        cmd_append(&cmd, "gdb", "-ex", "run", "--args", "./tech_support");
         if (!cmd_run(&cmd)) return 1;
     }
 
--- src/main.c
+++ src/main.c
@@ -1,12 +1,44 @@
 #include <raylib.h>
+#define STB_DS_IMPLEMENTATION
+#include <stb_ds.h>
+#define foreach(VARNAME, COLLECTION) \
+    for (__typeof__(COLLECTION) VARNAME = COLLECTION; \
+         VARNAME < COLLECTION + arrlen(COLLECTION); \
+         VARNAME++)
+
+// TODO:
+// - ecs.c, ecs_add, ecs_texture, ecs_position
+// - foreach -> stb_ds
+// - display.c, two modes, scaling
+
+typedef struct {
+    Texture *texture;
+    Vector2 *position;
+} Entity;
+
+static Entity *all_entities = NULL;
 
 int main()
 {
     InitWindow(400, 400, "Tech Support");
+
+    Entity e = {
+        .texture = malloc(sizeof(Texture)),
+        .position = malloc(sizeof(Vector2)),
+    };
+    *e.texture = LoadTexture("assets/sprites/humanoid.png");
+    e.position->x = 2;
+    e.position->y = 3;
+    arrput(all_entities, e);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawText("Hello, world!", 100, 100, 12, RAYWHITE);
+            foreach (e, all_entities) {
+                if (e->position && e->texture) {
+                    DrawTextureEx(*e->texture, *e->position, 0, 1, WHITE);
+                }
+            }
         EndDrawing();
     }
     CloseWindow();
